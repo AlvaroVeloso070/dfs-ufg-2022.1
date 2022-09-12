cbbc897e6f7e7d5d23655b78479f0ee7d22ce445
Álvaro Veloso <57870230+AlvaroVeloso070@users.noreply.github.com>
2022-09-11 21:51:34 -0300

    Update AboutPage.js

--- frontend-react/painel-paciente/src/pages/AboutPage/AboutPage.js
+++ frontend-react/painel-paciente/src/pages/AboutPage/AboutPage.js
@@ -28,7 +28,7 @@ export const AboutPage = () => {
             <div className='about-text'>
                 <h1>Sobre o projeto</h1>
                 <p>Este projeto foi desenvolvido para a disciplina de Desenvolvimento FullStack, no semestre de 2022/1 na Universidade Federal de Goiás.
-                <br/> Trabalhando com os conceitos da Arquitetura de Microsserviços, construímos duas API REST com SpringBoot, publicadas no <a href='heroku.com'>Heroku</a>. Quanto ao 
+                <br/> Trabalhando com os conceitos da Arquitetura de Microsserviços, construímos duas API REST com SpringBoot, publicadas no <a href='heroku.com'>Heroku</a> e banco de dados PostgreSQL da plataforma. Quanto ao 
                 FrontEnd, utilizamos React e seus tecnologias. Agradeceços especialmente ao professor <b>Reinaldo Junior</b>, o qual ministrou a disciplina, e nos orientou quanto à Arquitetura 
                 de Microsserviços e o SpringBoot.<br/>Caso tenha se interessado, abaixo segue o link do repositório no <b>GitHub</b>, sinta-se livre para implementá-lo.</p>
                 <a className='link-github' href="https://github.com/AlvaroVeloso070/painel-paciente-project"><FaGithub/> Repositório Painel Paciente</a>
